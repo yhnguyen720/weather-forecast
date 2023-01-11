8cd0752ad2d709c7f699859a5ba5872db8e7e62b
Yen Nguyen <112992331+yhnguyen720@users.noreply.github.com>
2023-01-10 19:48:49 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,15 @@
 var apiKey = "ff85c82019bc169c433101774a6ff69d";
+var today = dayjs();
 
+$("#date").text(today.format("dddd, MMMM D"));
+        
 $("#current-container").addClass("hide");
 $("#forecast-container").addClass("hide");
 
 $("#search").on("click", function (event) {
 	event.preventDefault();
 	var city = $("#city").val();
+
     $("#current-container").removeClass("hide");
 	$("#forecast-container").removeClass("hide");
 
@@ -19,7 +23,7 @@ $("#search").on("click", function (event) {
         let lat = response[0].lat;
         let lon = response[0].lon;
 
-        var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+        var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
 
         $.ajax({
             url: weatherUrl,
@@ -27,11 +31,67 @@ $("#search").on("click", function (event) {
         }).then(function(response){
             console.log(response);
             let temp = response.main.temp;
-            console.log(temp);
-        });
+            let icon = response.weather[0].icon;
+            let humidity = response.main.humidity;
+            let wind = response.wind.speed;
 
-        });
+            var iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-    });
+            $("#currentcity").text(city);
+            $("#icon").attr("src",iconUrl);
+            $(".temp").text(temp);
+            $(".wind").text(wind);
+            $(".humid").text(humidity);
+
+            var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&cnt=5&appid=" + apiKey + "&units=imperial";
+
+            $.ajax({
+                url: forecastUrl,
+                method: "GET",
+            }).then(function(response){
+                console.log(response);
+
+                const tempList = response.list.map((item) => {
+                    return {
+                        temp: item.main.temp
+                    }
+                })
+                console.log(tempList);
+
+                const iconList = response.list.map((item) => {
+                    return {
+                        icon: item.weather[0].icon
+                    }
+                })
+                console.log(iconList);
+
+                const humidList = response.list.map((item) => {
+                    return {
+                        humid: item.main.humidity
+                    }
+                })
+                console.log(humidList);
+
+                const windList = response.list.map((item) => {
+                    return {
+                        wind: item.wind.speed
+                    }
+                })
+                console.log(windList);
+    
+
+
+
+
+
+
+                })
+
+                
+
+
+            })
+        });
+        });
 
 
