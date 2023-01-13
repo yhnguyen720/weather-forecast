8483857929de1279d74cdfc1361803ee95329c15
Yen Nguyen <112992331+yhnguyen720@users.noreply.github.com>
2023-01-12 18:32:06 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,13 @@ var apiKey = "ff85c82019bc169c433101774a6ff69d";
 var today = dayjs();
 
 $("#date").text(today.format("dddd, MMMM D"));
-        
 $("#current-container").addClass("hide");
-$("#forecast-container").addClass("hide");
-
+        
 $("#search").on("click", function (event) {
 	event.preventDefault();
 	var city = $("#city").val();
 
     $("#current-container").removeClass("hide");
-	$("#forecast-container").removeClass("hide");
 
     var geoUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1" + "&appid=" + apiKey;
 
@@ -23,62 +20,60 @@ $("#search").on("click", function (event) {
         let lat = response[0].lat;
         let lon = response[0].lon;
 
-        var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+        var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&cnt=6&appid=" + apiKey + "&units=imperial";
 
         $.ajax({
             url: weatherUrl,
             method: "GET",
         }).then(function(response){
-            console.log(response);
-            let temp = response.main.temp;
-            let icon = response.weather[0].icon;
-            let humidity = response.main.humidity;
-            let wind = response.wind.speed;
-
-            var iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-
-            $("#currentcity").text(city);
-            $("#icon").attr("src",iconUrl);
-            $(".temp").text(temp);
-            $(".wind").text(wind);
-            $(".humid").text(humidity);
-
-            var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&cnt=5&appid=" + apiKey + "&units=imperial";
-
-            $.ajax({
-                url: forecastUrl,
-                method: "GET",
-            }).then(function(response){
-                console.log(response);
-
-                const tempList = response.list.map((item) => {
-                    return {
-                        temp: item.main.temp
-                    }
-                })
-                console.log(tempList);
-
-                const iconList = response.list.map((item) => {
-                    return {
-                        icon: item.weather[0].icon
-                    }
-                })
-                console.log(iconList);
-
-                const humidList = response.list.map((item) => {
-                    return {
-                        humid: item.main.humidity
-                    }
-                })
-                console.log(humidList);
-
-                const windList = response.list.map((item) => {
-                    return {
-                        wind: item.wind.speed
-                    }
-                })
-                console.log(windList);
     
+            const weatherList = response.list.map((item) => {
+                return {
+                    temp: item.main.temp,
+                    icon: item.weather[0].icon,
+                    humid: item.main.humidity,
+                    wind: item.wind.speed
+                }
+            })
+
+            var iconUrl = "http://openweathermap.org/img/wn/" + weatherList[0].icon + ".png"
+
+            $("#current-container").addClass("card border-primary p-2 shadow")
+            $("#current-container").append($("<h5>").addClass("text-capitalize").text(city));
+            $("#current-container").append($("<img>").addClass("icon").attr("src", iconUrl));
+            $("#current-container").append($("<p1>").text("Temperature: " + weatherList[0].temp + " \u00B0F"));
+            $("#current-container").append($("<p1>").text("Humidity: " + weatherList[0].humid + "%"));
+            $("#current-container").append($("<p1>").text("Wind Speed: " + weatherList[0].wind + " MPH"));
+
+            
+
+            $.each(weatherList, function(i) {
+                if (i !== 0)
+                    {
+                        var forecasticonUrl = "http://openweathermap.org/img/wn/" + weatherList[i].icon + ".png"
+
+                        const forecast = 
+                            `<div class="card">
+                                <h5> ${date} </h5>
+                                <img class = "icon" src="${forecasticonUrl}" />
+                                <p1> ${weatherList[i].temp} \u00B0F </p1>
+                                <p1> ${weatherList[i].humid} % </p1>
+                                <p1> ${weatherList[i].wind} MPH </p1>
+                            </div>`
+                    
+                        $("#forecast-container").append(forecast);
+                    }
+            })
+
+            
+          
+           
+
+            
+
+        
+            
+            
 
 
 
@@ -86,12 +81,8 @@ $("#search").on("click", function (event) {
 
 
                 })
-
-                
-
-
             })
         });
-        });
+
 
 
